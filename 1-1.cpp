b87c8655756f54d3c5848741b08acac60e4be1c9
Cyber-SiKu <Cyber-SiKu@outlook.com>
2019-06-28 22:21:49 +0800

    2-2 优化算法

--- 1-1.cpp
+++ 1-1.cpp
@@ -5,11 +5,15 @@ const int MAX = 100000;
 
 int min_child_array(const int array[],int size);
 int max(const int array[], int size);
+bool all_non_positive(const int array[], int size);
+
+int MaxSubSum(int array[], int len);
 
 int main(int argc, char **argv){
     int N = 0;
     int array[MAX] = {0};
     cin>>N;
+    int flag = true; // 记录全员是否是负数
     for(int i = 0; i < N; ++i){
         cin >> array[i];
     }
@@ -21,44 +25,49 @@ int min_child_array(const int array[],int size) {
     // 把所有连续的正整数加在一起，负数加在一起形成新的数组
     // 新的数组中的负数和左右两边的数字进行比较，如果两边数大于该数，三数相加
     // 重复上述步骤，直至不在增加
-    int answer[MAX] = {0};
-    int count = 0;
-    for (int i = 0, j = 0; i < size; ++i) {
-        if (answer[j]*array[i] < 0) {
-            // 当前数字和正在计算的符号不同
-            ++j;
-            ++count;
+   
+    if (!all_non_positive(array,size))
+    {   
+        int answer[MAX] = {0};
+        int count = 0;
+        for (int i = 0, j = 0; i < size; ++i) {
+            if (answer[j]*array[i] < 0) {
+                // 当前数字和正在计算的符号不同
+                ++j;
+                ++count;
+            }
+            answer[j] += array[i];
         }
-        answer[j] += array[i];
-    }
-    ++count;
-    // std::cout << "answer:\n";
-    //  for (int i = 0; i < count; ++i) {
-    //     std::cout << answer[i] << std::endl;
-    // }
+        ++count;
+        // std::cout << "answer:\n";
+        //  for (int i = 0; i < count; ++i) {
+        //     std::cout << answer[i] << std::endl;
+        // }
 
-    for(int i = 0;i < count; ++i) {
-        if (i > 0 && i < count){
-            if(answer[i] < 0) {
-               if((answer[i-1] + answer[i] > 0) 
-                 &&(answer[i+1]+ answer[i] > 0)) {
-                   answer[i-1] = answer[i-1] + answer[i] + answer[i+1];
-                   answer[i] = answer[i+1] = 0;
-                   if(answer[count-1] > 0){
-                       answer[i] = answer[count-2];
-                       answer[i+1] = answer[count-1];
-                   } else {
-                       answer[i] = answer[count-1];
-                       answer[i+1] = answer[count-2];
-                   }
-                   i -= 2;
-                   count -= 2;
-                   continue;
+        for(int i = 0;i < count; ++i) {
+            if (i > 0 && i < count){
+                if(answer[i] < 0) {
+                if((answer[i-1] + answer[i] > 0) 
+                    &&(answer[i+1]+ answer[i] > 0)) {
+                    answer[i-1] = answer[i-1] + answer[i] + answer[i+1];
+                    answer[i] = answer[i+1] = 0;
+                    if(answer[count-1] > 0){
+                        answer[i] = answer[count-2];
+                        answer[i+1] = answer[count-1];
+                    } else {
+                        answer[i] = answer[count-1];
+                        answer[i+1] = answer[count-2];
+                    }
+                    i -= 2;
+                    count -= 2;
+                    continue;
+                    }
                 }
             }
         }
+        return max(answer, count);
     }
-    return max(answer, count);
+    return max(array, size);
 }
 
 int max(const int array[], int size) {
@@ -71,4 +80,53 @@ int max(const int array[], int size) {
         }
     }
     return max;
+}
+
+bool all_non_positive(const int array[], int size) {
+    bool ret = true;
+    for (size_t i = 0; i < size; i++)
+    {
+        if (array[i] > 0)
+        {
+            ret = false;
+            break;
+        }
+    }
+    return ret;    
+}
+
+int MaxSubSum(int array[], int len) {
+    // 最优算法
+    /*
+    当数字有负有正时，和最大的子序列的第一个元素一定是正数
+    当全为非负数时一定是最大元素
+     */
+    if (!all_non_positive(array, len))
+    {
+        /* 当数字有负有正时 */
+        int i;
+	    int MaxSum = 0;
+	    int CurSum = 0;
+	    for(i=0;i<len;i++)
+	    {
+		    CurSum += array[i];
+		    if(CurSum > MaxSum)
+            {
+                MaxSum = CurSum;
+            }
+		    //如果累加和出现小于0的情况，
+		    //则和最大的子序列肯定不可能包含前面的元素，
+		    //这时将累加和置0，从下个元素重新开始累加
+		    if(CurSum < 0)
+            {
+			    CurSum = 0;
+            }
+	    }
+	    return MaxSum;
+    } else
+    {
+        return max(array, len);
+    }
+    
+    
 }
---
+++ 2-1.cpp
@@ -0,0 +1,94 @@
+/* 一般算法，遍历矩阵，计算每一个0点到1点的距离之和，返回其中的最小值*/
+/* 优化算法：分房子和空地两种类型，只记录房子的坐标 */
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+#define MAX 100
+
+void input_array(int array[][MAX],const int & size);
+// 输入数据
+
+std::vector<int> all_distance(int array[][MAX],const int & size);
+// 返回从各个0点到1点距离之和
+
+int distance_to_other(int array[][MAX], const int & size, const int & i, const int & j);
+// 返回点i,j到1点的距离之和
+
+int min_distance(std::vector<int> distances);
+
+int main(int argc, char **argv) 
+{
+    int array[MAX][MAX];
+    int N;
+    std::cin >> N;
+    input_array(array, N);
+    std::cout << min_distance(all_distance(array, N)) << std::endl;
+    return 0;
+}
+
+void input_array(int array[][MAX],const int & size) 
+{
+    for(int i = 0; i < size; ++i)
+    {
+        for(int j = 0; j < size; ++j) 
+        {
+            std::cin >> array[i][j];
+        }
+    }
+}
+
+std::vector<int> all_distance(int array[][MAX],const int & size)
+{
+    std::vector<int> distances;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (array[i][j] == 0)
+            {
+                int distance = distance_to_other(array, size, i, j);
+                distances.push_back(distance);
+            }
+        }
+        
+    }
+    return distances;
+}
+
+int distance_to_other(int array[][MAX], const int & size, const int & i, const int & j) {
+    int distance = 0;
+    for (int i_ = 0; i_ < size; i_++)
+    {
+        for (int j_ = 0; j_ < size; j_++)
+        {
+            if ((i != i_) || (j != j_))
+            {
+               if (array[i_][j_] == 1)
+                {
+                    distance = distance + abs(i-i_) + abs(j-j_);
+                }
+            }
+        }   
+    }
+    return distance;
+}
+
+int min_distance(std::vector<int> distances)
+{
+    int ret = -1;
+    if (!distances.empty())
+    {
+        ret = *distances.begin();
+        for (auto i = distances.begin()+1; i != distances.end(); i++)
+        {
+            if(ret > *i) 
+            {
+                ret = *i;
+            }
+        }
+        
+    }
+    return ret;
+}
