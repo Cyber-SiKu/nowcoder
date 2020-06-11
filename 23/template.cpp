17c3739c378b2e3cc3b8b69bb130a037679dc866
Cyber-SiKu <Cyber-SiKu@outlook.com>
2020-06-11 21:45:20 +0800

    23 start

---
+++ 23/template.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+private:
+    /* data */
+public:
+    Solution(/* args */);
+    ~Solution();
+};
+
+Solution::Solution(/* args */)
+{
+}
+
+Solution::~Solution()
+{
+}
+
+int main(int argc, char* argv[])
+{
+    Solution s;
+
+    return 0;
+}
