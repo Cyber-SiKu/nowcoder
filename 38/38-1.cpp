d2bfb06503dd483f541eb3cf1412e608d6375542
Cyber-SiKu <Cyber-SiKu@outlook.com>
2020-06-26 15:36:19 +0800

    38 finished

--- 38/38-1.cpp
+++ 38/38-1.cpp
@@ -1,18 +1,54 @@
+/**
+ * 运行时间：4ms
+ * 占用内存：480k
+*/
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 private:
-    /* data */
+    long min;
+    string path;
+
 public:
-    Solution(/* args */);
+    Solution(const vector<vector<long>>& data);
     ~Solution();
     friend ostream& operator<<(ostream& os, const Solution& s);
 };
 
-Solution::Solution(/* args */)
+Solution::Solution(const vector<vector<long>>& data)
 {
+    min = 0;
+    for (const vector<long>& i : data) {
+        long min_step = i[0];
+        long min_pos = 0;
+        for (size_t j = 1; j < 4; j++) {
+            if (min_step > i[j]) {
+                min_step = i[j];
+                min_pos = j;
+            }
+        }
+        this->min += min_step;
+        switch (min_pos) {
+        case 0:
+            this->path.append("E");
+            break;
+        case 1:
+            this->path.append("S");
+            break;
+        case 2:
+            this->path.append("W");
+            break;
+        case 3:
+            this->path.append("N");
+            break;
+        default:
+            break;
+        }
+    }
 }
 
 Solution::~Solution()
@@ -21,13 +57,28 @@ Solution::~Solution()
 
 ostream& operator<<(ostream& os, const Solution& s)
 {
+    os << s.min << endl
+       << s.path << endl;
     return os;
 }
 
 int main(int argc, char* argv[])
 {
-    Solution s;
+    int T, n;
+    cin >> T;
+    for (int z = 0; z < T; z++) {
+        cin >> n;
+        vector<vector<long>> data(n, vector<long>(4, 0));
+        for (size_t i = 0; i < 4; i++) {
+            for (size_t j = 0; j < n; j++) {
+                cin >> data[j][i];
+            }
+        }
 
-    cout << s << endl;
+        Solution s(data);
+
+        cout << s;
+    }
+
     return 0;
 }
