ab96e19831302b8deb535ec6ecf8f4a840d24dec
Cyber-SiKu <Cyber-SiKu@outlook.com>
2020-06-28 16:39:02 +0800

    40 finished

--- 40/40-1.cpp
+++ 40/40-1.cpp
@@ -1,18 +1,35 @@
+/**
+ * 运行时间：5ms
+ * 占用内存：492k
+*/
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 private:
-    /* data */
+    int outs;
+
 public:
-    Solution(/* args */);
+    Solution(int n, int m);
     ~Solution();
     friend ostream& operator<<(ostream& os, const Solution& s);
 };
 
-Solution::Solution(/* args */)
+Solution::Solution(int n, int m)
 {
+    vector<int> dp(m + 1, 0); // dp[i] 求和等于i时组合数
+    for (int i = n; i >= 1; i--) {
+        for (int j = m; j >= 1; j--) {
+            if (dp[j] != 0 && i + j <= m) {
+                // dp[j] 可以组合成功
+                dp[i + j] += dp[j];
+            }
+        }
+        dp[i] += 1;
+    }
+    this->outs = dp[m];
 }
 
 Solution::~Solution()
@@ -21,12 +38,15 @@ Solution::~Solution()
 
 ostream& operator<<(ostream& os, const Solution& s)
 {
+    os << s.outs;
     return os;
 }
 
 int main(int argc, char* argv[])
 {
-    Solution s;
+    int n, m;
+    cin >> n >> m;
+    Solution s(n, m);
 
     cout << s << endl;
     return 0;
