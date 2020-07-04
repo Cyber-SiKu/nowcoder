b743d682ba52b74c49ad3f0bfc3a742f42ab0402
Cyber-SiKu <Cyber-SiKu@outlook.com>
2020-07-04 11:37:04 +0800

    49 finished

--- 49/49-1.cpp
+++ 49/49-1.cpp
@@ -1,18 +1,40 @@
+/**
+ * 运行时间：4ms
+ * 占用内存：376k
+*/
+
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 class Solution {
 private:
-    /* data */
+    string outs;
+
 public:
-    Solution(/* args */);
+    Solution(const string& str);
     ~Solution();
     friend ostream& operator<<(ostream& os, const Solution& s);
+
+    // 返回 str 是否对称
+    bool symmetry(const string& str);
 };
 
-Solution::Solution(/* args */)
+Solution::Solution(const string& str)
 {
+    int length = str.size();
+    while (length > 1) {
+        for (size_t i = 0, e_i = str.size(); i + length <= e_i; i++) {
+            string substr = str.substr(i, length);
+            if (this->symmetry(substr)) {
+                outs = substr;
+                return;
+            }
+        }
+        length--;
+    }
+    outs.push_back(str[0]);
 }
 
 Solution::~Solution()
@@ -21,12 +43,28 @@ Solution::~Solution()
 
 ostream& operator<<(ostream& os, const Solution& s)
 {
+    os << s.outs;
     return os;
+}
+
+bool Solution::symmetry(const string& str)
+{
+    bool ret = true;
+    for (size_t i = 0, j = str.size() - 1; i <= j; i++, j--) {
+        if (str[i] != str[j]) {
+            ret = false;
+            break;
+        }
+    }
+
+    return ret;
 }
 
 int main(int argc, char* argv[])
 {
-    Solution s;
+    string str;
+    cin >> str;
+    Solution s(str);
 
     cout << s << endl;
     return 0;
