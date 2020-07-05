77e8547baecef735c37f0ade42d8ec6be3d96103
Cyber-SiKu <Cyber-SiKu@outlook.com>
2020-07-05 15:58:23 +0800

    50 start

---
+++ 52-OR142/52-1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+private:
+    /* data */
+public:
+    Solution(/* args */);
+    ~Solution();
+    friend ostream& operator<<(ostream& os, const Solution& s);
+};
+
+Solution::Solution(/* args */)
+{
+}
+
+Solution::~Solution()
+{
+}
+
+ostream& operator<<(ostream& os, const Solution& s)
+{
+    return os;
+}
+
+int main(int argc, char* argv[])
+{
+    Solution s;
+
+    cout << s << endl;
+    return 0;
+}
