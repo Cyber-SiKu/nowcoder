034ae5c1f3e6fe8a07a6355edb1d5c66e5ec564a
Cyber-SiKu <Cyber-SiKu@outlook.com>
2020-07-07 10:32:54 +0800

    55 finished

---
+++ 55-OR139/55-2.cpp
@@ -0,0 +1,59 @@
+/**
+ * 运行时间：4ms
+ * 占用内存：504k
+*/
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+private:
+    unsigned out;
+
+public:
+    Solution(const vector<int>& prices, const vector<pair<int, int>>& goods);
+    ~Solution();
+    friend ostream& operator<<(ostream& os, const Solution& s);
+};
+
+Solution::Solution(const vector<int>& prices, const vector<pair<int, int>>& goods)
+    : out(0)
+{
+    for (const pair<int, int>& i : goods) {
+        out += i.second * prices[i.first];
+    }
+}
+
+Solution::~Solution()
+{
+}
+
+ostream& operator<<(ostream& os, const Solution& s)
+{
+    os << s.out;
+    return os;
+}
+
+int main(int argc, char* argv[])
+{
+    int n;
+    cin >> n;
+    vector<int> prices(n + 1, 0);
+    for (size_t i = 1; i <= n; i++) {
+        cin >> prices[i];
+    }
+
+    int m;
+    cin >> m;
+    vector<pair<int, int>> goods(m, make_pair(0, 0));
+    for (size_t i = 0; i < m; i++) {
+        cin >> goods[i].first >> goods[i].second;
+    }
+
+    Solution s(prices, goods);
+
+    cout << s << endl;
+    return 0;
+}
