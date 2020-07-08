9d5bea2c5ea3d3ce8fdaebe2ba01f1393e3ddc26
Cyber-SiKu <Cyber-SiKu@outlook.com>
2020-07-08 22:22:43 +0800

    56-1

--- 56-OR137/56-1.cpp
+++ 56-OR137/56-1.cpp
@@ -1,20 +1,30 @@
+#include <cstring>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
+const int MAXSIZE = 10000;
 class Solution {
 private:
     int out;
+    int map[MAXSIZE][MAXSIZE];
+    int man_number;
+    int woman_number;
 
 public:
     Solution(const vector<vector<int>>& man, const vector<vector<int>>& woman);
     ~Solution();
     friend ostream& operator<<(ostream& os, const Solution& s);
+    void translate2Matrix(const vector<vector<int>>& man, const vector<vector<int>>& woman);
 };
 
 Solution::Solution(const vector<vector<int>>& man, const vector<vector<int>>& woman)
+    : man_number(man.size())
+    , woman_number(woman.size())
 {
+    memset(this->map, 0, sizeof(this->map));
+    translate2Matrix(man, woman);
 }
 
 Solution::~Solution()
@@ -25,6 +35,21 @@ ostream& operator<<(ostream& os, const Solution& s)
 {
     os << s.out;
     return os;
+}
+
+void Solution::translate2Matrix(const vector<vector<int>>& man, const vector<vector<int>>& woman)
+{
+    for (size_t i = 0, e_i = man.size(); i < e_i; ++i) {
+        for (size_t j = 0, e_j = man[i].size(); j < e_j; ++j) {
+            this->map[i][j]++;
+        }
+    }
+
+    for (size_t i = 0, e_i = woman.size(); i < e_i; ++i) {
+        for (size_t j = 0, e_j = woman[i].size(); j < e_j; ++j) {
+            this->map[j][i]++;
+        }
+    }
 }
 
 int main(int argc, char* argv[])
