0e5f2906cae80f89d3e5b0dbffcad5d535b6408f
Cyber-SiKu <Cyber-SiKu@outlook.com>
2020-07-12 20:27:55 +0800

    65 finished

--- 65-OR126/65-1.cpp
+++ 65-OR126/65-1.cpp
@@ -1,18 +1,42 @@
+/**
+ * 运行时间：3ms
+ * 占用内存：376k
+*/
 #include <iostream>
+#include <map>
+#include <string>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 private:
-    /* data */
+    const int MAXDAY = 5;
+    const int MAXCOURSE = 10;
+    vector<pair<string, vector<string>>> outs;
+
 public:
-    Solution(/* args */);
+    Solution(const map<string, vector<string>>& data);
     ~Solution();
     friend ostream& operator<<(ostream& os, const Solution& s);
 };
 
-Solution::Solution(/* args */)
+Solution::Solution(const map<string, vector<string>>& data)
 {
+    for (size_t i = 0; i < MAXDAY; i++) {
+        for (size_t j = 0; j < MAXCOURSE; j++) {
+            string tmp;
+            tmp.push_back(i + '0');
+            tmp.push_back(j + '0');
+            if (data.find(tmp) != data.end()) {
+                if (data.at(tmp).size() > 1) {
+                    /* 课程有冲突 */
+                    outs.push_back(make_pair(tmp, data.at(tmp)));
+                }
+            }
+        }
+    }
 }
 
 Solution::~Solution()
@@ -21,11 +45,32 @@ Solution::~Solution()
 
 ostream& operator<<(ostream& os, const Solution& s)
 {
+    if (s.outs.size() == 0) {
+        os << "YES";
+    } else {
+        for (const pair<string, vector<string>>& i : s.outs) {
+            os << i.first;
+            for (const string& j : i.second) {
+                os << " " << j;
+            }
+            os << endl;
+        }
+    }
+
     return os;
 }
 
 int main(int argc, char* argv[])
 {
-    cout << Solution() << endl;
+    map<string, vector<string>> data;
+    int n;
+    cin >> n;
+    for (size_t i = 0; i < n; i++) {
+        string day, course;
+        cin >> day >> course;
+        data[day].push_back(course);
+    }
+
+    cout << Solution(data);
     return 0;
 }
