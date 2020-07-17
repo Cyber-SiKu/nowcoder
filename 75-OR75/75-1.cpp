32acfd5b71d1ec7304ff2a9555d5ce036cc3da3b
Cyber-SiKu <Cyber-SiKu@outlook.com>
2020-07-17 18:14:21 +0800

    75完成输入

--- 75-OR75/75-1.cpp
+++ 75-OR75/75-1.cpp
@@ -1,3 +1,9 @@
+/**
+ * 运行时间：8ms
+ * 占用内存：632k
+*/
+
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
@@ -7,6 +13,7 @@ class Solution {
 private:
     int M, N;
     vector<vector<int>> map;
+    int out;
 
 public:
     Solution(/* args */);
@@ -25,10 +32,33 @@ Solution::~Solution()
 {
 }
 
-void Solution::slove() {}
+void Solution::slove()
+{
+    vector<vector<int>> dp(this->M, vector<int>(this->N, 0)); // 保存到 (i,j) 上一不所需的最少动力
+    for (int i = this->M - 1; i >= 0; --i) {
+        for (int j = this->N - 1; j >= 0; --j) {
+            if (i == this->M - 1 && j == this->N - 1) {
+                dp[i][j] = max(1, 0 - this->map[i][j]);
+                continue;
+            }
+            if (i == this->M - 1 && j != this->N - 1) {
+                dp[i][j] = max(1, dp[i][j + 1] - this->map[i][j]);
+                continue;
+            }
+            if (j == this->N - 1 && i != this->M - 1) {
+                dp[i][j] = max(1, dp[i + 1][j] - this->map[i][j]);
+                continue;
+            }
+            dp[i][j] = min(max(1, dp[i][j + 1] - this->map[i][j]), max(1, dp[i + 1][j] - this->map[i][j]));
+        }
+    }
+
+    this->out = dp[0][0];
+}
 
 ostream& operator<<(ostream& os, const Solution& s)
 {
+    os << s.out;
     return os;
 }
 istream& operator>>(istream& is, Solution& s)
