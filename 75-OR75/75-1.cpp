20dfb771ea98e826a1cc825f42d41846fffe84a7
Cyber-SiKu <Cyber-SiKu@outlook.com>
2020-07-17 16:33:26 +0800

    75 start

---
+++ 75-OR75/75-1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+private:
+    /* data */
+public:
+    Solution(/* args */);
+    ~Solution();
+    void slove();
+
+    friend ostream& operator<<(ostream& os, const Solution& s);
+    friend istream& operator>>(istream& is, Solution& s);
+};
+
+Solution::Solution(/* args */)
+{
+}
+
+Solution::~Solution()
+{
+}
+
+void Solution::slove() { }
+
+ostream& operator<<(ostream& os, const Solution& s)
+{
+    return os;
+}
+istream& operator>>(istream& is, Solution& s)
+{
+    return is;
+}
+
+int main(int argc, char* argv[])
+{
+    Solution s;
+    cin >> s;
+    s.slove();
+    cout << s << endl;
+    return 0;
+}
