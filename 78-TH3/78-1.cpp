6cda102259c392e4ddd95e7cd89c4f0772aaec22
Cyber-SiKu <Cyber-SiKu@outlook.com>
2020-07-18 21:09:02 +0800

    78-3

--- 78-TH3/78-1.cpp
+++ 78-TH3/78-1.cpp
@@ -1,10 +1,18 @@
 /**
  * 算法思想：
- *  最苯的方法：挨个试
-*/
-
-/**
- * 内存超出限制
+ *  可能出现的情况
+ *     0 | 1 | 2 | 3
+ *  k0     2   3   5
+ *  k1 2       3   5
+ *  k2 2   3       5
+ *  k3 2   3   5
+ * 
+ * 取A=\matrix{ 0, 2, 3, 5 \\ 
+ *              2, 0, 3, 5 \\
+ *              2, 3, 0, 5 \\
+ *              2, 3, 5, 0}
+ * 
+ * 然后sum(pinv(A)*[str2-str1])
 */
 #include <iostream>
 #include <queue>
@@ -14,7 +22,7 @@ using namespace std;
 
 class Solution {
 private:
-    const vector<int> ADD_NUM = { 2, 3, 5 };
+    int A_[4][4] = { { -12, 3, 3, 3 }, { 7, -8, 2, 2 }, { 5, 5, -5, 1 }, { 3, 3, 3, -3 } };
 
 public:
     /**
@@ -23,31 +31,20 @@ public:
      * @param s2 string字符串 表示目标的字符串
      * @return int整型
      */
+    bool isCorrect()
+    {
+    }
     int solve(string s1, string s2)
     {
-        queue<string> qs;
-        qs.push(s1);
-        int count = 0; // 计算变换了第几次
-        while (!qs.empty()) {
-            count++;
-            string s = qs.front();
-            qs.pop();
-            for (size_t i = 0; i < s.size(); i++) {
-                /* 选择的位置为i */
-                string replace(s);
-                for (size_t j = 0, k = 0; j < s.size(); j++) {
-                    if (j == i) {
-                        continue;
-                    }
-                    replace[j] = (replace[j] - 'a' + ADD_NUM[k]) % 26 + 'a'; // 替换
-                    k++;
-                }
+        int a[4] = { 0 };
+        for (size_t i = 0; i < 4; i++) {
+            a[i] = (s2[i] - s1[i]);
+        }
 
-                if (replace == s2) {
-                    return count;
-                } else {
-                    qs.push(replace);
-                }
+        int ans[4] = { 0 };
+        for (size_t i = 0; i < 4; i++) {
+            for (size_t j = 0; j < 4; j++) {
+                ans[i] += (A_[i][j] * a[j]);
             }
         }
     }
@@ -55,6 +52,6 @@ public:
 
 int main(int argc, char* argv[])
 {
-    cout << Solution().solve("aaaa", "ccgk") << endl;
+    cout << Solution().solve("fjdj", "jlas") << endl;
     return 0;
 }
