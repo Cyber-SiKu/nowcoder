dd8c7baae9d939fd00a473a77023889e6b8eaff2
Cyber-SiKu <Cyber-SiKu@outlook.com>
2020-07-19 21:02:46 +0800

    不满足每首歌都要播放一次

--- 80-OR120/80-1.cpp
+++ 80-OR120/80-1.cpp
@@ -1,10 +1,17 @@
+/**
+ * 不满足每首歌都要播放一次
+*/
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 private:
-    /* data */
+    long N, M, P;
+    long out;
+    const long MOD = 1000000007l;
+
 public:
     Solution(/* args */);
     ~Solution();
@@ -22,14 +29,32 @@ Solution::~Solution()
 {
 }
 
-void Solution::slove() { }
+void Solution::slove()
+{
+    if (M > N) {
+        out = 0;
+        return;
+    }
+
+    long dp = N;
+    for (size_t i = 1; i < P; i++) {
+        if (i <= M) {
+            dp = (dp * (N - i)) % MOD;
+        } else {
+            dp = (dp * (N - M)) % MOD;
+        }
+    }
+    out = dp;
+}
 
 ostream& operator<<(ostream& os, const Solution& s)
 {
+    os << s.out;
     return os;
 }
 istream& operator>>(istream& is, Solution& s)
 {
+    is >> s.N >> s.M >> s.P;
     return is;
 }
 
