86800e9812663dff6a3fa60fe75a3594f9f3db81
Cyber-SiKu <Cyber-SiKu@outlook.com>
2020-07-20 17:00:20 +0800

    82

---
+++ 82-HJ11/82-1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution {
+private:
+    int n;
+    string out;
+
+public:
+    Solution(/* args */);
+    ~Solution();
+    void slove();
+
+    friend ostream& operator<<(ostream& os, const Solution& s);
+    friend istream& operator>>(istream& is, Solution& s);
+};
+
+Solution::Solution(/* args */)
+{
+}
+
+Solution::~Solution()
+{
+}
+
+void Solution::slove()
+{
+    while (n != 0) {
+        out.push_back(n % 10 + '0');
+        n /= 10;
+    }
+}
+
+ostream& operator<<(ostream& os, const Solution& s)
+{
+    os << s.out;
+    return os;
+}
+istream& operator>>(istream& is, Solution& s)
+{
+    is >> s.n;
+    return is;
+}
+
+int main(int argc, char* argv[])
+{
+    Solution s;
+    cin >> s;
+    s.slove();
+    cout << s << endl;
+    return 0;
+}
